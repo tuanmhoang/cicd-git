53cd4919d5defa68ba2e3314330150973ad955b8
Tuan Hoang <hoangminhtuan0505@gmail.com>
2022-02-19 18:20:30 +0700

    add minus

--- simple-calculator/src/com/tuanmhoang/calculator/Calculator.java
+++ simple-calculator/src/com/tuanmhoang/calculator/Calculator.java
@@ -2,8 +2,12 @@ package com.tuanmhoang.calculator;
 
 public class Calculator {
 
-    public static Integer sum(Integer a, Integer b){
+    public static Integer sum(Integer a, Integer b) {
         return a + b;
+    }
+
+    public static Integer minus(Integer a, Integer b) {
+        return a - b;
     }
 
 }
