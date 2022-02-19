f6ebf95617f25f568dccfc308dea539a247d02ce
Tuan Hoang <hoangminhtuan0505@gmail.com>
2022-02-19 18:19:20 +0700

    change to java

--- simple-calculator/src/com/tuanmhoang/calculator/Calculator.groovy
+++
@@ -1,6 +0,0 @@
-package com.tuanmhoang.calculator
-
-def sum(a,b){
-  return a+b
-}
-
---
+++ simple-calculator/src/com/tuanmhoang/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.tuanmhoang.calculator;
+
+public class Calculator {
+
+    public static Integer sum(Integer a, Integer b){
+        return a + b;
+    }
+
+}
