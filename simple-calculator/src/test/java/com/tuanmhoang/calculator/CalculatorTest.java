9757615fffb4c94a2450e5577e00fd971d6d09bf
Tuan Hoang <hoangminhtuan0505@gmail.com>
2022-02-19 18:35:37 +0700

    test for multiply

--- simple-calculator/src/test/java/com/tuanmhoang/calculator/CalculatorTest.java
+++ simple-calculator/src/test/java/com/tuanmhoang/calculator/CalculatorTest.java
@@ -6,14 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
-   @Test
-   public void givenTwoNumber_whenSumIsCalled_thenReturnResult(){
-      assertEquals(5, Calculator.sum(2,3));
-   }
+    @Test
+    public void givenTwoNumber_whenSumIsCalled_thenReturnResult() {
+        assertEquals(5, Calculator.sum(2, 3));
+    }
 
-   @Test
-   public void givenTwoNumber_whenMinusIsCalled_thenReturnResult(){
-      assertEquals(-1, Calculator.minus(2,3));
-   }
+    @Test
+    public void givenTwoNumber_whenMinusIsCalled_thenReturnResult() {
+        assertEquals(-1, Calculator.minus(2, 3));
+    }
+
+    @Test
+    public void givenTwoNumber_whenMultiplyIsCalled_thenReturnResult() {
+        assertEquals(6, Calculator.minus(2, 3));
+    }
 
 }
