c1b0018c55bdca84ab942a470fd8a86444ee60a0
Tuan Hoang <hoangminhtuan0505@gmail.com>
2022-02-21 11:42:47 +0700

    add logs

--- simple-calculator/src/main/java/com/tuanmhoang/calculator/Calculator.java
+++ simple-calculator/src/main/java/com/tuanmhoang/calculator/Calculator.java
@@ -2,22 +2,32 @@ package com.tuanmhoang.calculator;
 
 import java.math.BigInteger;
 import java.util.function.BiFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Calculator {
 
+    private static Logger logger = LoggerFactory.getLogger(Calculator.class);
+
     public static BigInteger sum(Integer a, Integer b) {
         BiFunction<Integer, Integer, BigInteger> func = (x, y) -> BigInteger.valueOf(x + y);
-        return func.apply(a, b);
+        BigInteger result = func.apply(a, b);
+        logger.info("Result = {}", result);
+        return result;
     }
 
     public static BigInteger minus(Integer a, Integer b) {
         BiFunction<Integer, Integer, BigInteger> func = (x, y) -> BigInteger.valueOf(x - y);
-        return func.apply(a, b);
+        BigInteger result = func.apply(a, b);
+        logger.info("Result = {}", result);
+        return result;
     }
 
     public static BigInteger multiply(Integer a, Integer b) {
         BiFunction<Integer, Integer, BigInteger> func = (x, y) -> BigInteger.valueOf(x * y);
-        return func.apply(a, b);
+        BigInteger result = func.apply(a, b);
+        logger.info("Result = {}", result);
+        return result;
     }
 
 }
--- simple-calculator/src/test/java/com/tuanmhoang/calculator/CalculatorTest.java
+++ simple-calculator/src/test/java/com/tuanmhoang/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.tuanmhoang.calculator;
 
+import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,17 +9,17 @@ public class CalculatorTest {
 
     @Test
     public void givenTwoNumber_whenSumIsCalled_thenReturnResult() {
-        assertEquals(5, Calculator.sum(2, 3));
+        assertEquals(BigInteger.valueOf(5), Calculator.sum(2, 3));
     }
 
     @Test
     public void givenTwoNumber_whenMinusIsCalled_thenReturnResult() {
-        assertEquals(-1, Calculator.minus(2, 3));
+        assertEquals(BigInteger.valueOf(-1), Calculator.minus(2, 3));
     }
 
     @Test
     public void givenTwoNumber_whenMultiplyIsCalled_thenReturnResult() {
-        assertEquals(6, Calculator.multiply(2, 3));
+        assertEquals(BigInteger.valueOf(6), Calculator.multiply(2, 3));
     }
 
 }
