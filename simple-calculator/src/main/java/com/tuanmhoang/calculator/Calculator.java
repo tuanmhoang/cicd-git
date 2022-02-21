941631d8f5940f75ad0557e9200c75e425f81563
Tuan Hoang <hoangminhtuan0505@gmail.com>
2022-02-21 10:41:23 +0700

    support BigInteger

--- simple-calculator/src/main/java/com/tuanmhoang/calculator/Calculator.java
+++ simple-calculator/src/main/java/com/tuanmhoang/calculator/Calculator.java
@@ -1,21 +1,22 @@
 package com.tuanmhoang.calculator;
 
+import java.math.BigInteger;
 import java.util.function.BiFunction;
 
 public class Calculator {
 
-    public static Integer sum(Integer a, Integer b) {
-        BiFunction<Integer, Integer, Integer> func = (x, y) -> x + y;
+    public static BigInteger sum(Integer a, Integer b) {
+        BiFunction<Integer, Integer, BigInteger> func = (x, y) -> BigInteger.valueOf(x + y);
         return func.apply(a, b);
     }
 
-    public static Integer minus(Integer a, Integer b) {
-        BiFunction<Integer, Integer, Integer> func = (x, y) -> x - y;
+    public static BigInteger minus(Integer a, Integer b) {
+        BiFunction<Integer, Integer, BigInteger> func = (x, y) -> BigInteger.valueOf(x - y);
         return func.apply(a, b);
     }
 
-    public static Integer multiply(Integer a, Integer b) {
-        BiFunction<Integer, Integer, Integer> func = (x, y) -> x * y;
+    public static BigInteger multiply(Integer a, Integer b) {
+        BiFunction<Integer, Integer, BigInteger> func = (x, y) -> BigInteger.valueOf(x * y);
         return func.apply(a, b);
     }
 
